10becaf902e944d7a16c8a336e05a13ddc44da2e
Bithe <farzana.hossain5@gmail.com>
2024-02-16 21:11:35 +0600

    play

---
+++ js/apha-clash.js
@@ -0,0 +1,15 @@
+// function play(){
+//     const homeSection = document.getElementById('home');
+
+//     homeSection.classList.add('hidden');
+
+//     const playgroundSection = document.getElementById('playground');
+//     playgroundSection.classList.remove('hidden');
+// }
+
+
+function play(){
+    const homeSection = hideElementById('home');
+
+    const playgroundSection = showElementById('playground');
+}
