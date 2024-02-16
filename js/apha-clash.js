57ad46c10f8697d733c8d394ec08a11dd4f41437
Bithe <farzana.hossain5@gmail.com>
2024-02-17 04:12:38 +0600

    playground

--- js/apha-clash.js
+++ js/apha-clash.js
@@ -7,9 +7,35 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function play() {
+  const homeSection = hideElementById("home");
 
-function play(){
-    const homeSection = hideElementById('home');
-
-    const playgroundSection = showElementById('playground');
+  const playgroundSection = showElementById("playground");
+  playTheGame();
 }
+
+function playTheGame() {
+  const alphabet = getARandomAlphabet();
+
+  const CurrentAlphabet = document.getElementById("alphabet");
+  CurrentAlphabet.innerText = alphabet;
+
+  // generate a random alphabet
+  // getARandomAphabet();
+  setBackgroundColor(alphabet);
+}
+
+document.addEventListener("keyup", function (event) {
+  const playerPressed = event.key;
+
+  const currentAlphabet = document.getElementById("alphabet");
+  const currentAlphabetText = currentAlphabet.innerText;
+
+  const currentAlphabetToLower = currentAlphabetText.toLowerCase();
+
+  if (playerPressed === currentAlphabetToLower) {
+    console.log("ok");
+    playTheGame();
+    removeBackgrounColor(currentAlphabetToLower);
+  }
+});
--- js/utility.js
+++ js/utility.js
@@ -7,3 +7,25 @@ function showElementById(id){
     const element = document.getElementById(id);
     element.classList.remove('hidden');
 }
+function setBackgroundColor(alphaId){
+    const alphabetId = document.getElementById(alphaId);
+    alphabetId.classList.add('bg-orange-400');
+}
+
+function removeBackgrounColor(alphaId){
+    const alphabetId = document.getElementById(alphaId);
+    alphabetId.classList.remove('bg-orange-400');
+}
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const random = Math.random()*25;
+    const randomIndex = Math.round(random);
+    const randomAlphabets = alphabets[randomIndex];
+
+    // console.log(randomAlphabets)
+    return randomAlphabets;
+}
+
